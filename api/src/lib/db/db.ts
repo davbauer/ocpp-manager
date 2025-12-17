f74ff1e1220e6b06987d88a996f665428558d3c8
David Bauer <89662560+davbauer@users.noreply.github.com>
2025-12-17 16:56:03 +0100

    Adjust connection pooling

--- api/src/lib/db/db.ts
+++ api/src/lib/db/db.ts
@@ -6,6 +6,10 @@ import { env } from "../globals/env";
 
 const pool = new Pool({
   connectionString: env.get("DATABASE_URL"),
+  max: 20,
+  min: 2,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 export const db = new Kysely<DB>({
